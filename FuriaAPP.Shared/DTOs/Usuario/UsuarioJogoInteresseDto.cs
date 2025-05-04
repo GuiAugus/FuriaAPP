8c66c280e786aa7b45c34678713e80f7ec086b34
Gui <augusto.g@outlook.com.br>
2025-05-04 16:21:03 -0300

    fix: correcao na filtragem da pagina noticia

--- FuriaAPP.Shared/DTOs/Usuario/UsuarioJogoInteresseDto.cs
+++ FuriaAPP.Shared/DTOs/Usuario/UsuarioJogoInteresseDto.cs
@@ -1,8 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace FuriaAPP.Shared.DTOs.Usuario
 {
     public class UsuarioJogoInteresseDto
     {
+        [JsonPropertyName("jogoId")]
         public int JogoId { get; set; }
+
+        [JsonPropertyName("nomeJogo")]
         public string NomeJogo { get; set; } = string.Empty;
     }
 }
